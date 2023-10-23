8b17217229a712a20ddff59bd5d178245e41a49f
Matthew Adams <matthew.adams@endjin.com>
2023-10-23 10:17:26 +0100

    Added optimized non-terminating pipeline builds.

--- PipelineExperiment/Pipeline.cs
+++ PipelineExperiment/Pipeline.cs
@@ -7,6 +7,34 @@ public static class Pipeline
         return static state => ValueTask.FromResult(state);
     }
 
+    public static PipelineStep<TState> Build<TState>(params PipelineStep<TState>[] steps)
+    {
+        return async state =>
+        {
+            TState currentResult = state;
+            foreach (var step in steps)
+            {
+                currentResult = await step(currentResult).ConfigureAwait(false);
+            }
+
+            return currentResult;
+        };
+    }
+
+    public static PipelineStep<TState> Build<TState>(params SyncPipelineStep<TState>[] steps)
+    {
+        return state =>
+        {
+            TState currentResult = state;
+            foreach (var step in steps)
+            {
+                currentResult = step(currentResult);
+            }
+
+            return ValueTask.FromResult(currentResult);
+        };
+    }
+
     public static PipelineStep<TState> Build<TState>(Predicate<TState> shouldTerminate, params PipelineStep<TState>[] steps)
     {
         return async state =>
