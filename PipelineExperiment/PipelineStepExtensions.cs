61b0901e1155a6e18d5794fa2ec10f627dc46ba1
Matthew Adams <matthew.adams@endjin.com>
2023-10-24 19:38:24 +0100

    Added ICanFail interface for steps which may not succeed. Added Retry() operator Added OnError() operator

--- PipelineExperiment/PipelineStepExtensions.cs
+++ PipelineExperiment/PipelineStepExtensions.cs
@@ -12,6 +12,67 @@ namespace PipelineExperiment;
 public static class PipelineStepExtensions
 {
     /// <summary>
+    /// An operator which provides the ability to retry a step which might fail.
+    /// </summary>
+    /// <typeparam name="TState">The type of the state.</typeparam>
+    /// <typeparam name="TError">The type of the error details.</typeparam>
+    /// <param name="step">The step to execute.</param>
+    /// <param name="shouldRetry">A predicate which determines if the step should be retried.</param>
+    /// <param name="beforeRetry">An step to carry out before retrying. This is commonly an asynchronous delay, but can be used to return
+    /// an updated version of the state before retyring the action (e.g. incrementing an execution count.</param>
+    /// <returns>A <see cref="PipelineStep{TState}"/> which, when executed, will execute the step, choose the appropriate pipeline, based on the result,
+    /// and execute it using the result.</returns>
+    public static PipelineStep<TState> Retry<TState, TError>(this PipelineStep<TState> step, Predicate<TState> shouldRetry, PipelineStep<TState>? beforeRetry = null)
+        where TState : struct, ICanFail<TState, TError>
+        where TError : struct
+    {
+        return async state =>
+        {
+            TState currentState = state;
+
+            while (true)
+            {
+                currentState = await step(currentState).ConfigureAwait(false);
+                if (currentState.ExecutionStatus == PipelineStepStatus.Success || !shouldRetry(currentState))
+                {
+                    return currentState;
+                }
+
+                if (beforeRetry is not null)
+                {
+                    currentState = await beforeRetry(currentState).ConfigureAwait(false);
+                }
+            }
+        };
+    }
+
+    /// <summary>
+    /// An operator which provides the ability to choose a step to run if the bound step fails.
+    /// </summary>
+    /// <typeparam name="TState">The type of the state.</typeparam>
+    /// <typeparam name="TError">The type of the error details.</typeparam>
+    /// <param name="step">The step to execute.</param>
+    /// <param name="onError">The step to execute if the step fails.</param>
+    /// <returns>A <see cref="PipelineStep{TState}"/> which, when executed, will execute the step, and, if an error occurs,
+    /// execute the error step before returning the final result.</returns>
+    public static PipelineStep<TState> OnError<TState, TError>(
+        this PipelineStep<TState> step,
+        PipelineStep<TState> onError)
+        where TState : struct, ICanFail<TState, TError>
+        where TError : struct
+    {
+        return step.Bind(async state =>
+        {
+            if (state.ExecutionStatus != PipelineStepStatus.Success)
+            {
+                return await onError(state).ConfigureAwait(false);
+            }
+
+            return state;
+        });
+    }
+
+    /// <summary>
     /// An operator that binds the output of one <see cref="PipelineStep{TState}"/> to another <see cref="PipelineStep{TState}"/>
     /// provided by a <paramref name="selector"/> function.
     /// </summary>
